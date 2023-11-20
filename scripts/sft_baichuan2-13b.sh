e444b5cfbdb16d00789b75222e8c6de3a6c8853a
zhuoyiwei <zhuoyiwei@fhxk.com>
2023-11-21 05:06:59 +0800

    添加 Baichuan 模型训练启动脚本

---
+++ scripts/sft_baichuan2-13b.sh
@@ -0,0 +1,22 @@
+MODLE_DIR=/workspace/models/baichuan2-13b-chat-20230906
+OUTPUT_DIR=./temp
+
+CUDA_VISIBLE_DEVICES=1 python train.py \
+    --stage sft \
+    --model_name_or_path $MODLE_DIR \
+    --do_train \
+    --dataset alpaca_zh \
+    --template default \
+    --finetuning_type lora \
+    --lora_target W_pack,o_proj \
+    --output_dir $OUTPUT_DIR \
+    --overwrite_cache \
+    --per_device_train_batch_size 4 \
+    --gradient_accumulation_steps 4 \
+    --lr_scheduler_type cosine \
+    --logging_steps 10 \
+    --save_steps 1000 \
+    --learning_rate 5e-5 \
+    --num_train_epochs 3.0 \
+    --plot_loss \
+    --fp16
