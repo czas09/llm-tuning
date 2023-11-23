4e8ac985be376e3975cbd57f626eb2fc46bb4a6f
zhuoyiwei <zhuoyiwei@fhxk.com>
2023-11-24 04:00:53 +0800

    新增一些训练脚本

---
+++ scripts/sft_qlora_chatglm3-6b.sh
@@ -0,0 +1,23 @@
+MODLE_DIR=/workspace/models/chatglm3-6b-20231027
+OUTPUT_DIR=./temp/chatglm
+
+CUDA_VISIBLE_DEVICES=1 python train.py \
+    --stage sft \
+    --model_name_or_path $MODLE_DIR \
+    --do_train \
+    --dataset alpaca_zh \
+    --template default \
+    --finetuning_type lora \
+    --lora_target query_key_value \
+    --quantization_bit 4 \
+    --output_dir $OUTPUT_DIR \
+    --overwrite_cache \
+    --per_device_train_batch_size 4 \
+    --gradient_accumulation_steps 4 \
+    --lr_scheduler_type cosine \
+    --logging_steps 10 \
+    --save_steps 1000 \
+    --learning_rate 5e-5 \
+    --num_train_epochs 3.0 \
+    --plot_loss \
+    --fp16
